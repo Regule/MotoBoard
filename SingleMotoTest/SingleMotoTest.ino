0c25249f3c96d16a1a2a76a3e03c43951bae3c63
regule <regule@zoho.com>
2020-12-04 10:19:14 +0100

    Tested encoder, wierd behaviour on reverse

--- SingleMotoTest/SingleMotoTest.ino
+++ SingleMotoTest/SingleMotoTest.ino
@@ -1,7 +1,8 @@
 
 //------------------------------------GENERAL CONFIGURATION----------------------------------------
+#define DEBUG_MODE 
 #define SERIAL_BAUD_RATE 9600
-#define ODOMETRY_CHECK_INTERVAL 100 // in milisceonds
+#define ODOMETRY_UPDATE_PERIOD 100 // in milisceonds
 #define VELOCITY_MEASURE_INTERVAL 100 //in miliseconds
 
 //----------------------------------ROBOT PHYSICAL PROPERTIES--------------------------------------
@@ -32,24 +33,29 @@ public:
 DefaultHandler default_handler;
 InterruptHandler* interrupt_handlers[INTERRUPT_COUNT];
 
-void irs_0(){Serial.print("WTF");interrupt_handlers[0]->handle_interrupt();}
+void irs_0(){interrupt_handlers[0]->handle_interrupt();}
 void irs_1(){interrupt_handlers[1]->handle_interrupt();}
 
 void(* irs_functions[])() = {irs_0, irs_1};
 
-void initialize_interrupt_handlers(){
-	for(int i=0; i<INTERRUPT_COUNT; i++){
-		interrupt_handlers[i] = &default_handler;
-		attachInterrupt(i,irs_functions[i],RISING);
-	}
-}
 
 void assign_interrupt_handler(int interrupt_id, InterruptHandler* handler, int mode){
+#ifdef DEBUG_MODE
+	Serial.print("Attaching function ");
+	unsigned long ptr = (unsigned long) handler;
+	Serial.print(ptr);
+	Serial.print(" to interrupt ");
+	Serial.println(interrupt_id);
+#endif
 	interrupt_handlers[interrupt_id] = handler;
 	attachInterrupt(interrupt_id, irs_functions[interrupt_id], mode);
 }
 
-
+void initialize_interrupt_handlers(){
+	for(int i=0; i<INTERRUPT_COUNT; i++){
+		assign_interrupt_handler(i, &default_handler, RISING);
+	}
+}
 //=================================================================================================
 //                                         MOVEMENT CLASS
 //=================================================================================================
@@ -60,12 +66,23 @@ public:
 
 	Movement(){
 		velocity = 0.0;
-		forward = false;
+		forward = true;
 	}
 
 	Movement(double velocity, bool forward){
 		this->velocity = velocity;
 		this->forward = forward;
+	}
+
+	void send_as_ascii(){
+		Serial.print("Velocity=");
+		Serial.print(velocity);
+		Serial.print(" direction=");
+		if(forward){
+			Serial.print("FORWARD");
+		}else{
+			Serial.print("REVERSE");
+		}
 	}
 };
 
@@ -103,6 +120,7 @@ public:
 		unsigned long now = millis();
 		movement.velocity = (double)(impulse_count)*INTERRUPTS_TO_MM / (now-timestamp);
 		timestamp = now;
+		impulse_count = 0;
 	}
 
 	Movement get_movement(){
@@ -125,8 +143,42 @@ private:
 };
 
 //=================================================================================================
+//                                       TESTS AND DEBUG
+//=================================================================================================
+#ifdef DEBUG_MODE
+
+void setup(){
+	Serial.begin(SERIAL_BAUD_RATE);
+	initialize_interrupt_handlers();
+}
+
+void simple_encoder_test(){
+	Encoder encoder(2,4);
+	while(true){
+		delay(ODOMETRY_UPDATE_PERIOD);
+		encoder.update_velocity_data();
+		encoder.get_movement().send_as_ascii();
+		Serial.println();
+	}
+}
+
+void loop(){
+	Serial.println("Select test :");
+	Serial.println("1 - Simple encoder test");
+	while(Serial.available()==0){
+	}
+	int selection = Serial.parseInt();
+	switch(selection){
+		case 1: simple_encoder_test();break;
+		default: Serial.println("Test do not exist.");break;
+	}
+}
+#endif
+
+//=================================================================================================
 //                                          MAIN CODE
 //=================================================================================================
+#ifndef DEBUG_MODE
 
 void setup(){
 	Serial.begin(SERIAL_BAUD_RATE);
@@ -135,3 +187,5 @@ void setup(){
 
 void loop(){
 }
+
+#endif
