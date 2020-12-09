750594b1216200f7bad100a6d84ec45f5a5d9ec4
regule <regule@zoho.com>
2020-12-09 12:02:23 +0100

    Added bubble sort as a overhead test

--- SingleSonarTest/SingleSonarTest.ino
+++ SingleSonarTest/SingleSonarTest.ino
@@ -3,6 +3,50 @@
 #define ECHO 2  // This must be an interrupt pin
 
 
+class Bubbler{
+private:
+	long *data;
+	int size;
+	bool finished;
+	unsigned long timestamp_start;
+	unsigned long timestamp_end;
+
+public:
+	
+	Bubbler(int size){
+		randomSeed(micros());
+		this->size = size;
+		this->data = (long*)malloc(size);
+		this->finished = false;
+		for(int i=0; i<size; i++){
+			this->data[i] = random();
+		}
+		timestamp_start = micros();
+		timestamp_end = micros();
+	}
+
+	bool step(){
+		if(finished) return true;
+		int changes = 0;
+		for(int i=0; i<size-1; i++){
+			if(data[i]>data[i+1]){
+				long tmp = data[i];
+				data[i] = data[i+1];
+				data[i+1] = tmp;
+				changes++;
+			}
+		}
+		if(changes == 0){
+			finished = true;
+			this->timestamp_end = micros();
+		}
+		return finished;
+	}
+
+    unsigned long get_execution_time(){
+		return timestamp_end - timestamp_start;
+	}
+};
 
 int int_from_serial(){
 	while(Serial.available() == 0){
@@ -38,6 +82,9 @@ void active_pool(){
 
 
 void loop(){
+	Serial.println("Enter size of sorted array:");
+	int array_size = int_from_serial();
+	Bubbler bubbler(array_size);
 	Serial.println("Choose mode :");
 	Serial.println("1 - Active pooling mode");
 	Serial.println("2 - Interrupt mode");
@@ -47,6 +94,9 @@ void loop(){
 		setup_active_pool();
 		while(!finished){
 			active_pool();
+			finished = bubbler.step();
 		}
+		Serial.print("Execution time : ");
+		Serial.println(bubbler.get_execution_time());
 	}
 }
