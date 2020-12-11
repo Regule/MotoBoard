aa8ac20039ef8913b7a5be77a8c680385179caae
regule <regule@zoho.com>
2020-12-11 12:05:05 +0100

    Added software debouncing

--- BumperTest/BumperTest.ino
+++ BumperTest/BumperTest.ino
@@ -1,7 +1,14 @@
 #define SERIAL_BAUDRATE 9600
 #define BUMPER_PIN 2
+#define BUMPER_DEBOUNCE_PERIOD 10
+
+unsigned long last_interrupt;
 
 void bumper_handler(){
+	if(millis()-last_interrupt<BUMPER_DEBOUNCE_PERIOD){
+		return;
+	}
+	last_interrupt = millis();
 	Serial.println("BUMP");
 }
 
@@ -9,6 +16,7 @@ void setup(){
 	Serial.begin(9600);
 	pinMode(BUMPER_PIN, INPUT);
 	attachInterrupt(digitalPinToInterrupt(BUMPER_PIN), bumper_handler, FALLING);
+	last_interrupt = 0;
 }
 
 void loop(){
