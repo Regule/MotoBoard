8810c9e565632e56d13119214602b0a93725578e
regule <regule@zoho.com>
2020-12-12 16:36:17 +0100

    Wrote interrupt handling

--- MotoBoard1/MotoBoard1.ino
+++ MotoBoard1/MotoBoard1.ino
@@ -30,8 +30,35 @@
 #define PIN_SONAR_RIGHT_TRIGGER 17
 #define PIN_SONAR_RIGHT_ECHO 20 // Must be external interrupt
 
+
 //=================================================================================================
-//                                          SETUP-LOOP
+//                                     INTERRUPT HANDLING
+//=================================================================================================
+#define INTERRUPT_COUNT 6
+
+class InterruptHandler{
+public:
+	virtual void handle_interrupt() = 0;	
+};
+
+InterruptHandler* interrupt_handlers[INTERRUPT_COUNT];
+
+void irs_0(){interrupt_handlers[0]->handle_interrupt();}
+void irs_1(){interrupt_handlers[1]->handle_interrupt();}
+void irs_2(){interrupt_handlers[2]->handle_interrupt();}
+void irs_3(){interrupt_handlers[3]->handle_interrupt();}
+void irs_4(){interrupt_handlers[4]->handle_interrupt();}
+void irs_5(){interrupt_handlers[5]->handle_interrupt();}
+
+void(* irs_functions[])() = {irs_0, irs_1, irs_2, irs_3, irs_4, irs_5};
+
+void assign_interrupt_handler(int interrupt_id, InterruptHandler* handler, int mode){
+	interrupt_handlers[interrupt_id] = handler;
+	attachInterrupt(interrupt_id, irs_functions[interrupt_id], mode);
+}
+
+//=================================================================================================
+//                               SETUP-LOOP + SERIAL HANDLING
 //=================================================================================================
 
 void setup(){
