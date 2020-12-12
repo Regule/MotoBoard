fc060f045c8f04ba5a439e1fd3e48f38e908c2eb
regule <regule@zoho.com>
2020-12-12 16:26:39 +0100

    Wrote pinout for moto-board

---
+++ MotoBoard1/MotoBoard1.ino
@@ -0,0 +1,43 @@
+//=================================================================================================
+//                                            CONFIG
+//=================================================================================================
+
+// Pinout
+#define PIN_ENCODER_LEFT_IMPULSE 2 // Must be external interrupt
+#define PIN_ENCODER_LEFT_DIRECTION 4
+
+#define PIN_ENCODER_RIGHT_IMPULSE 3 // Must be external interrupt
+#define PIN_ENCODER_RIGHT_DIRECTION 5 
+
+#define PIN_MOTOR_LEFT_FWD 6
+#define PIN_MOTOR_LEFT_REV 7
+#define PIN_MOTOR_LEFT_PWM 8
+
+#define PIN_MOTOR_RIGHT_FWD 9
+#define PIN_MOTOR_RIGHT_REV 10
+#define PIN_MOTOR_RIGHT_PWM 11
+
+#define PIN_BUMPER_LEFT 12
+#define PIN_BUMPER_CENTER 13
+#define PIN_BUMPER_RIGHT 14 
+
+#define PIN_SONAR_LEFT_TRIGGER 15
+#define PIN_SONAR_LEFT_ECHO 18 // Must be external interrupt
+
+#define PIN_SONAR_CENTER_TRIGGER 16
+#define PIN_SONAR_CENTER_ECHO 19 // Must be external interrupt
+
+#define PIN_SONAR_RIGHT_TRIGGER 17
+#define PIN_SONAR_RIGHT_ECHO 20 // Must be external interrupt
+
+//=================================================================================================
+//                                          SETUP-LOOP
+//=================================================================================================
+
+void setup(){
+
+}
+
+void loop(){
+
+}
